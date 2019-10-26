69ebb28dceb261e94e53ef6706a2170de3ea8806
Bruce Brookshire <bruce.t.brookshire@gmail.com>
2019-10-26 16:52:22 -0500

    UI improvements for the home page as well as data fetching organization

--- lib/bill_view.dart
+++ lib/bill_view.dart
@@ -4,8 +4,8 @@ import 'package:bill_md_mobile/bm_widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:bill_md_mobile/constants.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:simple_animations/simple_animations/controlled_animation.dart';
+import 'package:simple_animations/simple_animations/multi_track_tween.dart';
 
 class BillView extends StatefulWidget {
   @override
@@ -14,53 +14,44 @@ class BillView extends StatefulWidget {
 
 class _BillViewState extends State<BillView> {
   List<BillData> userAccounts;
+  bool loading;
 
   @override
   void initState() {
     super.initState();
 
-    fetchUserAccounts();
+    loading = false;
+    fetchBills();
   }
 
-  void tappedAddBill(_) {
-    Navigator.push(
-      context,
-      CupertinoPageRoute(
-        builder: (context) => AddBillView(),
-      ),
-    );
-  }
+  void tappedAddBill(_) => Navigator.push(
+        context,
+        CupertinoPageRoute(
+          builder: (context) => AddBillView(),
+        ),
+      );
 
-  List<BillData> getBillData( UserData){
-    return UserData.bills;
-  }
+  void fetchBills() async {
+    setState(() => loading = true);
 
-  void fetchUserAccounts() {
-    // We use futures here because of their readability, and simplicity in parsing errors.
-    Firestore.instance
-        .collection(FirebaseCollections.user_accounts)
-        .getDocuments()
-        // On completion, update UI
+    UserData.getUsers()
         .then(
-          (documents) =>
-              setState(() => this.userAccounts = getBillData(UserData.fromJson(documents.documents[0].data))),
+          (users) => setState(
+            () {
+              this.userAccounts = users.bills;
+              this.loading = false;
+            },
+          ),
         )
-        //Notice we are passing the print function as the param so that it just goes straight to the console. 
-        // During development, this is helpful for seeing errors. In the future, we will want to use 
-        // a package like (shameless plug for my package :) ) https://pub.dev/packages/dropdown_banner 
-        // to inform the user of a failure
         .catchError(print);
-    //We could've also done:
-    // .catchError((e) => print(e));
   }
 
   @override
   Widget build(BuildContext context) {
-
     // TODO Sandeep: look how we handle the case where we are still waiting for Firestore to load vs when it finishes
     // Determine our user body now to make further down more readable for now.
     // We can put this within the body later if we wanted.
-    final Widget contentCards = userAccounts == null
+    final Widget contentCards = loading
         // Waiting on Firestore to load
         ? Center(
             child: Column(
@@ -76,75 +67,87 @@ class _BillViewState extends State<BillView> {
             children: <Widget>[
               Expanded(
                 child: ListView(
-                  children: userAccounts
-                      .map((d) => CustomCard(
-                            title: d.accountNum,
-                            description: d.amountDue,
-                          ))
-                      .toList(),
+                  children: userAccounts.map((b) => _StatementCard(b)).toList(),
                 ),
               ),
             ],
           );
 
-    return Column(
+    final tween = MultiTrackTween([
+      Track("color1").add(Duration(seconds: 5),
+          ColorTween(begin: Color(0xFF90E2CD), end: Color(0xFF7CD3D0))),
+      Track("color2").add(Duration(seconds: 5),
+          ColorTween(begin: Color(0xFF7CD3D0), end: Color(0xFF90E2CD))),
+    ]);
+
+    final body = Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Container(
-          padding: EdgeInsets.symmetric(vertical: 24),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF90E2CD),
-                Color(0xFF7CD3D0),
-              ],
+        Icon(
+          Icons.account_balance,
+          color: Colors.white,
+          size: 48,
+        ),
+        Text(
+          'YOUR BILLS',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.fromLTRB(48, 12, 48, 0),
+          child: Text(
+            'Did you know this random fact about medical bills?',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w400,
             ),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Icon(
-                Icons.account_balance,
-                color: Colors.white,
-                size: 48,
-              ),
-              Text(
-                'YOUR BILLS',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600,
+        ),
+      ],
+    );
+
+    return Column(
+      children: [
+        ControlledAnimation(
+          playback: Playback.MIRROR,
+          tween: tween,
+          duration: tween.duration,
+          builder: (context, animation) {
+            return Container(
+              padding: EdgeInsets.symmetric(vertical: 24),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    animation["color1"],
+                    animation["color2"],
+                  ],
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(48, 12, 48, 0),
-                child: Text(
-                  'Did you know this random fact about medical bills?',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-            ],
-          ),
+              child: body,
+            );
+          },
         ),
         //Show our content
         Expanded(child: contentCards),
-        // TODO Sandeep: 
+        // TODO Sandeep:
         // We can use two nifty syntaxes found in dart 2.3 to minimize the complexity of building a list
         // 1.) The ... syntax flatmaps an array within an array
         // 2.) If statements conditionally build elements into lists
 
-        // Check https://medium.com/flutter-community/whats-new-in-dart-2-3-1a7050e2408d 
+        // Check https://medium.com/flutter-community/whats-new-in-dart-2-3-1a7050e2408d
         // For a great explanation of the powers of these operators when building UI
 
         // If there are no users, prompt to add (a bill/user/idk :) )
         //if (userAccounts == []) ...[
+        if ((userAccounts ?? []) == []) ...[
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 48, vertical: 16),
             child: Text(
@@ -165,8 +168,85 @@ class _BillViewState extends State<BillView> {
               ),
             ),
           ),
+        ]
         //],
       ],
+    );
+  }
+}
+
+class _StatementCard extends StatelessWidget {
+  final BillData billData;
+
+  _StatementCard(this.billData);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(7),
+          boxShadow: UIAssets.boxShadow,
+          color: Colors.white),
+      child: Row(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: BMColors.actionableBlue,
+              borderRadius: BorderRadius.circular(3),
+            ),
+            width: 44,
+            height: 44,
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Text(
+                    'Due: Nov. 19th',
+                    style: TextStyle(
+                        color: BMColors.alertOrange,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 2, bottom: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Text(
+                          'Statement 1',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w600, fontSize: 16),
+                        ),
+                        Text(
+                          '\$0 / \$2,5000.00',
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal, fontSize: 12, letterSpacing: -0.3),
+                        ),
+                      ],
+                    ),
+                  ),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(2),
+                    child: LinearProgressIndicator(
+                      value: 40 / 100,
+                      backgroundColor: BMColors.lightGrey,
+                      valueColor:
+                          AlwaysStoppedAnimation(BMColors.actionableBlue),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Icon(Icons.chevron_right)
+        ],
+      ),
     );
   }
 }
--- lib/userdata.dart
+++ lib/userdata.dart
@@ -1,20 +1,28 @@
+import 'package:bill_md_mobile/constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class UserData {
   final String userid;
   final List<BillData> bills;
 
   UserData({this.userid, this.bills});
 
-  factory UserData.fromJson(Map<dynamic, dynamic> parsedJson){
+  static Future<UserData> getUsers() {
+    return Firestore.instance
+        .collection(FirebaseCollections.user_accounts)
+        .getDocuments()
+        // On completion, update UI
+        .then(
+          (documents) => UserData.fromJson(documents.documents[0].data),
+        );
+  }
 
+  factory UserData.fromJson(Map<dynamic, dynamic> parsedJson) {
     var list = parsedJson['DataFields'] as List;
     print(list.runtimeType);
     List<BillData> billsList = list.map((i) => BillData.fromJson(i)).toList();
 
-    return UserData(
-      userid: parsedJson['userid'],
-      bills: billsList
-
-    );
+    return UserData(userid: parsedJson['userid'], bills: billsList);
   }
 }
 
@@ -24,14 +32,15 @@ class BillData {
   final String amountDue;
   //final DateTime dueDate;
 
-  BillData({this.accountNum, this.statementNum, this.amountDue});//, this.dueDate});
+  BillData(
+      {this.accountNum, this.statementNum, this.amountDue}); //, this.dueDate});
 
-  factory BillData.fromJson(Map<dynamic, dynamic> parsedJson){
-   return BillData(
-     accountNum:parsedJson['accountnum'],
-     statementNum:parsedJson['statementnum'],
-     amountDue:parsedJson['amountdue']
-     //dueDate:parsedJson['duedate']
-   );
+  factory BillData.fromJson(Map<dynamic, dynamic> parsedJson) {
+    return BillData(
+        accountNum: parsedJson['accountnum'],
+        statementNum: parsedJson['statementnum'],
+        amountDue: parsedJson['amountdue']
+        //dueDate:parsedJson['duedate']
+        );
   }
 }
